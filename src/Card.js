ef85dc49bbf90a3f6ba66eebc07df6188bf103cc
Michael Shkarubski <40541820+epamekids@users.noreply.github.com>
2018-11-26 20:27:41 +0300

    Creating React App and edtitng some files

---
+++ src/Card.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+
+class Card extends Component {
+  render() {
+    return (
+      <html>
+        <head>
+          <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons"  />
+          <link rel="stylesheet" href="https://code.getmdl.io/1.3.0/material.indigo-pink.min.css" />
+
+          <script defer src="https://code.getmdl.io/1.3.0/material.min.js"></script>
+        </head>
+        <body>
+
+        </body>
+      </html>
+    )
+  }
+}
+
+export default Card;
