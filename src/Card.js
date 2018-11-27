13c5bf3e09921951d839cede80162d562b520906
Michael Shkarubski <40541820+epamekids@users.noreply.github.com>
2018-11-27 19:33:19 +0300

    Edtiting React App

--- src/Card.js
+++ src/Card.js
@@ -3,17 +3,17 @@ import React, { Component } from 'react';
 class Card extends Component {
   render() {
     return (
-      <html>
-        <head>
-          <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons"  />
-          <link rel="stylesheet" href="https://code.getmdl.io/1.3.0/material.indigo-pink.min.css" />
-
-          <script defer src="https://code.getmdl.io/1.3.0/material.min.js"></script>
-        </head>
-        <body>
-
-        </body>
-      </html>
+      <div class="demo-card-wide mdl-card mdl-shadow--2dp">
+        <div class="mdl-card__title">
+          <h2 class="mdl-card__title-text">{this.props.title}</h2>
+        </div>
+        <div class="mdl-card__supporting-text">
+          {this.props.body}
+        </div>
+        <div class="mdl-card__actions mdl-card--border">
+          <a class="mdl-button mdl-button--colored mdl-js-button mdl-js-ripple-effect">Some Button</a>
+        </div>
+      </div>
     )
   }
 }
--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import App from './App';
 import Card from './Card';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<div><Card /></div>, document.getElementById('root'));
+ReactDOM.render(<div><Card img="https://www.cnews.cz/wp-content/uploads/2016/12/5/3/5/9/6///logo-hello-orkut-ico.png" title="Title" body="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris sagittis pellentesque lacus eleifend lacinia"/></div>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
