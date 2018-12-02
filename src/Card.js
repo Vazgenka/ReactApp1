ab63f06c0ed95d7c856d0f81a3b04506245bdb0f
Michael Shkarubski <40541820+epamekids@users.noreply.github.com>
2018-12-02 15:50:33 +0300

    Editing Card.js

--- src/Card.js
+++ src/Card.js
@@ -44,28 +44,6 @@ class Card extends Component {
             <a className="mdl-button mdl-button--colored mdl-js-button mdl-js-ripple-effect">Some Button</a>
           </div>
         </div>
-        <div className="demo-card-wide mdl-card mdl-shadow--2dp">
-          <div className="mdl-card__title" style={style2}>
-            <h2 className="mdl-card__title-text" style={h2}>{this.props.title2}</h2>
-          </div>
-          <div className="mdl-card__supporting-text">
-            {this.props.body2}
-          </div>
-          <div className="mdl-card__actions mdl-card--border">
-            <a className="mdl-button mdl-button--colored mdl-js-button mdl-js-ripple-effect">Some Button</a>
-          </div>
-        </div>
-        <div className="demo-card-wide mdl-card mdl-shadow--2dp">
-          <div className="mdl-card__title" style={style3}>
-            <h2 className="mdl-card__title-text" style={h2}>{this.props.title3}</h2>
-          </div>
-          <div className="mdl-card__supporting-text">
-            {this.props.body3}
-          </div>
-          <div className="mdl-card__actions mdl-card--border">
-            <a className="mdl-button mdl-button--colored mdl-js-button mdl-js-ripple-effect">Some Button</a>
-          </div>
-        </div>
       </div>
     )
   }
